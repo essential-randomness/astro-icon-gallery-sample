5f1589d2020a200da40e906ab6dc89fb3e2101f3
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-05-22 16:25:32 -0700

    get gallery to work by character

--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,16 +1,15 @@
 import path from "path";
-import {readdir} from 'fs/promises';
+import { readdir } from "fs/promises";
 
 export async function listDirectories(pth) {
-  const directories = (await readdir(pth, {withFileTypes: true}))
-    .filter(dirent => dirent.isDirectory())
-    .map(dir => dir.name);
+  const directories = (await readdir(pth, { withFileTypes: true }))
+    .filter((dirent) => dirent.isDirectory())
+    .map((dir) => dir.name);
 
   return directories;
 }
 
-export async function getIconImages(iconId: string) {;
-
+export async function getIconImages(iconId: string) {
   let images = import.meta.glob<{ default: ImageMetadata }>(
     "/src/content/gallery/**/*.{jpeg,jpg,png,gif}"
   );
@@ -22,26 +21,60 @@ export async function getIconImages(iconId: string) {;
   const resolvedImages = await Promise.all(
     Object.values(images).map((image) => image().then((mod) => mod.default))
   );
-  
+
   return resolvedImages;
 }
 
-export async function getIconSet(name: string) {
-	let images = import.meta.glob<{ default: ImageMetadata }>(
-	    "/src/content/gallery/**/*.{jpeg,jpg,png,gif}"
-	);
-	
-	let dirnames = await listDirectories("./src/content/gallery/kamen-rider-gotchard");
-	let names = Array.fromAsync(dirnames);
+function getCharacters(
+  resolvedImages: ImageMetadata[],
+  baseGalleryPath: string
+) {
+  const allFolders = resolvedImages
+    .map(({ src }) => {
+      const pathInGallery = src.replace("/@fs" + baseGalleryPath + "/", "");
+      const firstSlash = pathInGallery.indexOf("/");
+      if (firstSlash === -1) {
+        return null;
+      }
+      return pathInGallery.substring(0, firstSlash);
+    })
+    .filter((x) => x !== null);
 
-	const finalDirNames = Array.from(dirnames);
-	const getImages = getIconImages();
+  return Array.from(new Set(allFolders));
+}
 
-	const resolvedImages = await Promise.all(
-	    Object.values(images).map((image) => image().then((mod) => mod.default))
-	  );
+export async function getIconSet(filePath: string) {
+  const { dir, name } = path.parse(filePath);
+  const galleryPath = path.resolve(dir, name);
 
-	const finalImages = resolvedImages.filter(({ src }) => src.includes(name));
+  let images = import.meta.glob<{ default: ImageMetadata }>(
+    "/src/content/gallery/**/*.{jpeg,jpg,png,gif}"
+  );
 
-	return finalImages;
+  console.log(galleryPath);
+
+  // console.log(name);
+
+  // let dirnames = await listDirectories("./src/content/gallery/demo");
+  // let names = Array.fromAsync(dirnames);
+
+  // const finalDirNames = Array.from(dirnames);
+  // const getImages = getIconImages();
+
+  const resolvedImages = await Promise.all(
+    Object.values(images).map((image) => image().then((mod) => mod.default))
+  );
+  // console.log(resolvedImages.map((img) => img.src));
+
+  const characters = getCharacters(resolvedImages, galleryPath);
+
+  const characterImages: Record<string, ImageMetadata[]> = {};
+
+  for (const character of characters) {
+    characterImages[character] = resolvedImages.filter(({ src }) =>
+      src.startsWith("/@fs" + path.resolve(galleryPath, character))
+    );
+  }
+
+  return characterImages;
 }
