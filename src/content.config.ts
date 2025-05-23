c2e4d8aed07bc0fcdae0af393efe6ebe78229542
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-05-23 12:36:36 -0700

    more comments

--- src/content.config.ts
+++ src/content.config.ts
@@ -39,7 +39,7 @@ type ImagesSchema = z.ZodObject<{
   >;
 }> & {};
 
-async function withImagesSchema<T extends z.AnyZodObject>(
+function withImagesSchema<T extends z.AnyZodObject>(
   schemaFunction: (_: SchemaContext) => T,
   transformFunction?: (
     obj: z.infer<T> & z.infer<ImagesSchema>,
@@ -83,11 +83,8 @@ function withImages(loader: Loader) {
         return data.data;
       },
     });
-    console.dir(context.store.values(), { depth: null });
     await Promise.all(
       context.store.values().map(async (value) => {
-        console.dir("valoue in store");
-        console.dir(value);
         const loadedPromise = Promise.withResolvers();
         getAllImagesForFileEntry(value.filePath as string).then(
           async (images) => {
@@ -116,14 +113,32 @@ function withImages(loader: Loader) {
   return loader;
 }
 
+/**
+ * Assume a content directory with the following structure:
+ *
+ * series1.yml
+ * series2.yml
+ * /series1/["collection of images for series 1"]
+ * /series2/["collection of images for series 2"]
+ *
+ * We want to load all the images for each series in this content
+ * collection WITHOUT having to explicitly list every image in the
+ * YAML file. We also want to make this work regardless of loader type.
+ */
 const icons = defineCollection({
+  // We wrap our loader (in this case the default glob loader from Astro)
+  // with the `withImages` function. With this, each element in our
+  // collection loaded by the original loader will also have an
+  // additional `images` property with all the images in the corresponding directory.
   loader: withImages(
     astroGlobLoader({
       pattern: "**/*.yml",
       base: "./src/content/gallery",
     })
   ),
-  schema: await withImagesSchema(
+  // If we want type safety, we can wrap the schema with the `withImagesSchema`
+  // function, which will add the `images` property to our output.
+  schema: withImagesSchema(
     ({ image }) =>
       z.object({
         title: z.string(),
@@ -131,9 +146,13 @@ const icons = defineCollection({
         cover: image().optional(),
         characters: z.string().array().default([]),
       }),
+    // Optionally, we can also add a second argument to `withImagesSchema` which
+    // allows us to use the loaded images to set the value of properties in the final
+    // object using the loaded images.
     (result, allImages) => {
       result["characters"] = Array.from(
-        // The first folder in a path is the character name
+        // In this case, the images are divided by characters, which means we can
+        // use the first folder in the path to collect all the names of the characters
         new Set(allImages.map((img) => img.relativePath.split("/")[0]))
       );
     }
