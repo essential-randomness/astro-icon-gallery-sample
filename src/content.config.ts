d3b583635371dd65730cc5158607776214504ed8
Essential Randomness <essentialrandomn3ss@gmail.com>
2025-05-22 19:45:59 -0700

    not working

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,39 +1,16 @@
-import { defineCollection, z } from "astro:content";
-import { glob as astroGlobLoader, file } from "astro/loaders";
+import { defineCollection, z, type SchemaContext } from "astro:content";
+import {
+  glob as astroGlobLoader,
+  file,
+  type Loader,
+  type LoaderContext,
+} from "astro/loaders";
 import { glob, readFile } from "node:fs/promises";
 import { parse } from "yaml";
 
 import { picsLoader } from "./loaders/pics";
 import path from "node:path";
-
-// A loader that is exactly the loader passed in, but also has a
-// `images` property which loads all the images in the "gallery path"
-const loaderWithImages = withImageLoader(astroGlobLoader);
-defineCollection({
-  // give it the same thing as the astro loader does
-  loader: loaderWithImages("/src/gallery/**"),
-  schema: ({ image }) =>
-    withLoadedImages(
-      z.object({
-        title: z.string(),
-        description: z.string(),
-        cover: image().optional(),
-        characters: z
-          .array(
-            z.object({
-              name: z.string(),
-              icons: z.array(image()),
-            })
-          )
-          .default([]),
-      }),
-      (obj, images) => {
-        // any code to go from `obj` to the schema you want to have
-        // given the images
-        obj[characters] = getImageByCharacter(images);
-      }
-    ),
-});
+import { object } from "astro:schema";
 
 async function getAllImagesForFileEntry(entryFilePath: string) {
   const entryPath = path.parse(entryFilePath);
@@ -53,80 +30,246 @@ async function getAllImagesForFileEntry(entryFilePath: string) {
   }));
 }
 
-const icons = defineCollection({
-  loader: async () => {
-    const charactersFiles = await Array.fromAsync(
-      glob("src/content/gallery/*.yml")
-    );
-    const characterData = await Promise.all(
-      charactersFiles.map(async (charactersFile) => {
-        const fileContent = await readFile(charactersFile, "utf-8");
-        const parsedYaml = parse(fileContent);
-        const fileName = path.parse(charactersFile);
+function loaderWithImages(
+  ...params: Parameters<typeof astroGlobLoader>
+): Loader {
+  return {
+    name: astroGlobLoader.name + "-with-images",
+    // Called when updating the collection.
+    load: async (context): Promise<void> => {
+      console.log("NEW LOADER");
+      console.log("NEW LOADER");
+      console.log("NEW LOADER");
+      console.log("NEW LOADER");
+      console.log("NEW LOADER");
+      await astroGlobLoader(...params).load(context);
+      // All loader entries are from this current loader
+      await Promise.all(
+        context.store.values().map(async (value) => {
+          console.log(value);
+          const promise = await getAllImagesForFileEntry(
+            value.filePath as string
+          );
+          context.store.set({
+            id: value.id,
+            data: {
+              ...value,
+              images: await promise,
+            },
+          });
 
-        return {
-          id: fileName.name,
-          ...parsedYaml,
-          cover: parsedYaml.cover
-            ? `/@fs` + path.resolve("src/content/gallery/", parsedYaml.cover)
-            : undefined,
-          images: await getAllImagesForFileEntry(charactersFile),
-        };
+          return promise;
+        })
+      );
+      console.dir(context.store.values(), { depth: null });
+      console.log("BYE LOADER");
+      console.log("BYE LOADER");
+      console.log("BYE LOADER");
+      console.log("BYE LOADER");
+      console.log("BYE LOADER");
+      console.log("BYE LOADER");
+    },
+  };
+}
+
+type ImagesSchema = z.ZodObject<{
+  images: z.ZodArray<
+    z.ZodObject<{
+      image: ReturnType<SchemaContext["image"]>;
+      relativePath: z.ZodString;
+    }>
+  >;
+}> & {};
+async function withImagesSchema<T extends z.AnyZodObject>(
+  schemaFunction: (_: SchemaContext) => T,
+  transformFunction?: (
+    obj: z.infer<T> & ImagesSchema,
+    images: z.infer<ImagesSchema>["images"]
+  ) => void
+) {
+  return (context: SchemaContext) => {
+    const schema = schemaFunction(context);
+    const zodBase = z.object({
+      images: z.array(
+        z.object({
+          relativePath: z.string(),
+          image: context.image(),
+        })
+      ),
+    });
+    zodBase.extend(schema.shape);
+    if (transformFunction) {
+      zodBase.transform((obj) => {
+        transformFunction(
+          obj as unknown as z.infer<T> & ImagesSchema,
+          obj.images
+        );
+      });
+    }
+    return schema as T & ImagesSchema;
+  };
+}
+function withImages(loader: Loader) {
+  const oldLoad = loader.load;
+  loader.load = async (context: LoaderContext) => {
+    await oldLoad(context);
+    await Promise.all(
+      context.store.values().map(async (value) => {
+        const loadedPromise = Promise.withResolvers();
+        getAllImagesForFileEntry(value.filePath as string).then(
+          async (images) => {
+            const { digest, ...valueWithoutDigest } = value;
+            const newData = await context.parseData({
+              id: value.id,
+              data: {
+                ...valueWithoutDigest.data,
+                images: images,
+              },
+            });
+            context.store.set({
+              id: value.id,
+              data: newData,
+              digest: context.generateDigest(newData),
+            });
+            loadedPromise.resolve(images);
+          }
+        );
+
+        return loadedPromise.promise;
       })
     );
+  };
 
-    return characterData;
-  },
-  schema: ({ image }) =>
-    z
-      .object({
+  return loader;
+}
+
+// A loader that is exactly the loader passed in, but also has a
+// `images` property which loads all the images in the "gallery path"
+// const loaderWithImages = withImageLoader(astroGlobLoader);
+// defineCollection({
+//   // give it the same thing as the astro loader does
+//   loader: loaderWithImages("/src/gallery/**"),
+//   schema: ({ image }) =>
+//     withLoadedImages(
+//       z.object({
+//         title: z.string(),
+//         description: z.string(),
+//         cover: image().optional(),
+//         characters: z
+//           .array(
+//             z.object({
+//               name: z.string(),
+//               icons: z.array(image()),
+//             })
+//           )
+//           .default([]),
+//       }),
+//       (obj, images) => {
+//         // any code to go from `obj` to the schema you want to have
+//         // given the images
+//         obj[characters] = getImageByCharacter(images);
+//       }
+//     ),
+// });
+
+const icons = defineCollection({
+  loader: withImages(
+    astroGlobLoader({
+      pattern: "**/*.yml",
+      base: "./src/content/gallery",
+    })
+  ),
+  schema: await withImagesSchema(
+    ({ image }) =>
+      z.object({
         title: z.string(),
         description: z.string(),
         cover: image().optional(),
-        images: z
-          .array(
-            z.object({
-              relativePath: z.string(),
-              image: image(),
-            })
-          )
-          .default([]),
-        characters: z
-          .array(
-            z.object({
-              name: z.string(),
-              icons: z.array(image()),
-            })
-          )
-          .default([]),
-      })
-      .transform((obj) => {
-        const characterNames = Array.from(
-          new Set(
-            obj.images
-              .map((imageFilePath) => {
-                // Since the folder names inside the gallery are the character names, the
-                // character name will be the first folder in the returned paths.
-                return imageFilePath.relativePath.substring(
-                  0,
-                  imageFilePath.relativePath.indexOf("/")
-                );
-              })
-              .filter((name) => !!name)
-          )
-        );
-
-        const charactersIcons = characterNames.map((character) => ({
-          name: character,
-          icons: obj.images
-            .filter((src) => src.relativePath.startsWith(character))
-            .map((image) => image.image),
-        }));
-
-        obj["characters"] = charactersIcons;
-
-        return obj;
+        characters: z.string().array().default([]),
       }),
+    (object, images) => {
+      console.log(object);
+      object["characters"] = Array.from(
+        new Set(images.map((img) => img.relativePath.split("/")[0]))
+      );
+    }
+  ),
 });
+
+// const icons = defineCollection({
+//   loader: async () => {
+//     const charactersFiles = await Array.fromAsync(
+//       glob("src/content/gallery/*.yml")
+//     );
+//     const characterData = await Promise.all(
+//       charactersFiles.map(async (charactersFile) => {
+//         const fileContent = await readFile(charactersFile, "utf-8");
+//         const parsedYaml = parse(fileContent);
+//         const fileName = path.parse(charactersFile);
+
+//         return {
+//           id: fileName.name,
+//           ...parsedYaml,
+//           cover: parsedYaml.cover
+//             ? `/@fs` + path.resolve("src/content/gallery/", parsedYaml.cover)
+//             : undefined,
+//           images: await getAllImagesForFileEntry(charactersFile),
+//         };
+//       })
+//     );
+
+//     return characterData;
+//   },
+//   schema: ({ image }) =>
+//     z
+//       .object({
+//         title: z.string(),
+//         description: z.string(),
+//         cover: image().optional(),
+//         images: z
+//           .array(
+//             z.object({
+//               relativePath: z.string(),
+//               image: image(),
+//             })
+//           )
+//           .default([]),
+//         characters: z
+//           .array(
+//             z.object({
+//               name: z.string(),
+//               icons: z.array(image()),
+//             })
+//           )
+//           .default([]),
+//       })
+//       .transform((obj) => {
+//         const characterNames = Array.from(
+//           new Set(
+//             obj.images
+//               .map((imageFilePath) => {
+//                 // Since the folder names inside the gallery are the character names, the
+//                 // character name will be the first folder in the returned paths.
+//                 return imageFilePath.relativePath.substring(
+//                   0,
+//                   imageFilePath.relativePath.indexOf("/")
+//                 );
+//               })
+//               .filter((name) => !!name)
+//           )
+//         );
+
+//         const charactersIcons = characterNames.map((character) => ({
+//           name: character,
+//           icons: obj.images
+//             .filter((src) => src.relativePath.startsWith(character))
+//             .map((image) => image.image),
+//         }));
+
+//         obj["characters"] = charactersIcons;
+
+//         return obj;
+//       }),
+// });
 
 export const collections = { icons };
